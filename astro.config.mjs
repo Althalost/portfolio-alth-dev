456514b046c42425fa625d02865b4b0c9ceb31d3
Althalost <pablolopez101200@gmail.com>
2024-10-09 00:40:38 -0400

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://althalost.github.io',
+  base: 'portfolio-alth-dev',
 });
